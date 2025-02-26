5d859703e40ccd21d5ea6ff799aaecfb910d3066
dnlaxe <195729718+dnlaxe@users.noreply.github.com>
2025-02-26 11:27:05 +0900

    Add Exercism solution for: matrix

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(matrix) {
+    this.matrix = matrix.split('\n');
+  }
+
+  get rows() {
+    return this.matrix.map((row) => row.split(' ').map(Number));
+  }
+
+  get columns() {
+    return this.rows[0].map((_, index) => this.rows.map((row) => row[index]));
+  }
+}
