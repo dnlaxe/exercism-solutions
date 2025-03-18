9af60c7c179ebc2341a794176a535e6c4337e02b
dnlaxe <195729718+dnlaxe@users.noreply.github.com>
2025-03-18 13:42:00 +0900

    Add Exercism solution for: scrabble-score

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -3,15 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = (word) => {
-  const letterPoints = new Map([
-    ...'AEIOULNRST'.split('').map(letter => [letter, 1]),
-    ...'DG'.split('').map(letter => [letter, 2]),
-    ...'BCMP'.split('').map(letter => [letter, 3]),
-    ...'FHVWY'.split('').map(letter => [letter, 4]),
-    ...'K'.split('').map(letter => [letter, 5]),
-    ...'JX'.split('').map(letter => [letter, 8]),
-    ...'QZ'.split('').map(letter => [letter, 10]),
-]);
-  return [...word.toUpperCase()].reduce((acc, letter) => acc += letterPoints.get(letter),0);
-};
+export const score = (word) => 
+  [...word.toUpperCase()].reduce((acc, letter) => acc + (
+    new Map(
+      [, 'AEIOULNRST', 'DG', 'BCMP', 'FHVWY', 'K', , , 'JX', , 'QZ']
+      .flatMap((letters, points) => letters ? [...letters].map(letter => [letter, points]) : [])
+      ).get(letter)), 0);
