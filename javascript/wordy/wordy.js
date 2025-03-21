e51d6297a53425ed1db79914b10473ba837cc729
dnlaxe <195729718+dnlaxe@users.noreply.github.com>
2025-03-21 19:57:28 +0900

    Add Exercism solution for: wordy

---
+++ javascript/wordy/wordy.js
@@ -0,0 +1,35 @@
+//
+// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const answer = (question) => {
+  question = question.slice(8).slice(0, -1);
+
+  // empty array
+  if (question.length === 0) throw new Error('Syntax error');
+  
+  // a single number
+  if (!isNaN(question)) return Number(question);
+  
+  // invalid words
+  if (!/(plus|minus|divided by|multiplied by)/.test(question)) throw new Error('Unknown operation');
+  
+  // array: number symbol number etc
+  let matches = question.match(/(-?\d+|plus|minus|divided by|multiplied by)/g);
+  if (matches.length % 2 === 0) throw new Error('Syntax error')
+  if (matches.some((val, i) => (i % 2 === 0 && isNaN(Number(val)) || i % 2 === 1 && !isNaN(Number(val))))) throw new Error('Syntax error');
+  
+  return matches.reduce((acc, num, index) => {
+    if (num === 'plus') return acc + Number(matches[index + 1]);
+    if (num === 'minus') return acc - Number(matches[index + 1]);
+    if (num === 'divided by') return acc / Number(matches[index + 1]);
+    if (num === 'multiplied by') return acc * Number(matches[index + 1]);
+    return acc;
+  }, Number(matches[0]));
+}
+
+
+
+  
+
